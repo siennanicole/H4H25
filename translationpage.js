2a36fb1e3d219c613b915c8f24e4a27df48132dd
Trisha Ganesh <43594876+trishaganesh@users.noreply.github.com>
2025-02-15 15:57:49 -0800

    Update translationpage.js

--- translationpage.js
+++ translationpage.js
@@ -1,1 +1,15 @@
+function dyslexicFriendlyTxt(text) {
+    let textFormatting = text
+    .replace(/(\w{8,})/g, '<b>$1</b>')
+    .replace(/\n/g, '<br><br>');
 
+    let outputs = document.getElementById("dyslexic_friendly_text");
+    outputs.innerHTML = textFormatting;
+
+    outputs.style.fontFamily = "OpenDyslexic, Arial, Comic-Sans, sans-serif"
+    outputs.style.fontSize = "20px";
+    outputs.style.lineHeight = "2.0";
+    outputs.style.backgroundColor = "#f7f7f7";
+    outputs.style.padding = "15px";
+    outputs.style.borderRadius = "10px";
+}
