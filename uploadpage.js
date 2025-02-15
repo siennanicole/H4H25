671d1b61b9e4558503cc85569c43f432ef537977
siennanicole <160055092+siennanicole@users.noreply.github.com>
2025-02-15 12:19:01 -0800

    jss for upload page (submit button to translation page)

--- uploadpage.js
+++ uploadpage.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // get the form element
+    const form = document.querySelector("form");
 
+    // handel/listen for form submission
+    form.addEventListener("submit", function(event) {
+        // prevent default reload the page
+        event.preventDefault();
+        
+        // get the selected font
+        const selectedFont = document.getElementById("font-select").value;
+
+        // get the uploaded file 
+        const fileInput = document.getElementById("file-upload");
+        const file = fileInput.files[0]; 
+
+        if (!file) {
+            alert("Please upload a PDF file.");
+            return;
+        }
+
+        // store data in sessionStorage (data will persist as long as the user stays on the site)
+        sessionStorage.setItem("fileData", JSON.stringify(fileData));
+
+        // redirect to the translation page
+        window.location.href = "translationpage.html";
+    });
+});
