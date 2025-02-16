8a110ed1ea98446fdb3946802fd7ccbd56026a06
Adam-Tout <71945581+Adam-Tout@users.noreply.github.com>
2025-02-15 17:44:08 -0800

    Create pythonstartup.py

---
+++ pythonstartup.py
@@ -0,0 +1,6 @@
+import sys
+
+# Add your project directory to sys.path for interactive sessions (optional)
+project_home = '/home/h4h/mysite'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
